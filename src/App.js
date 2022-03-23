f9768da82ceb4f60ade8516b11a509b21645f11a
Abdallah Elmesery <abdallahelmesery@gmail.com>
2022-03-23 13:37:37 +0200

    update 1.37

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route  path="/" element={<Home />} />
+        <Route  path="/" exact element={<Home />} />
         <Route  path='about' element={<About />} />
         <Route  path='services' element={<Services />} />
         <Route  path='serviceName' element={<ServiceName />} />
